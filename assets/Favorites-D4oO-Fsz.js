809410c90630b5facd964dc0ff8f2a24261582a0
Nataliya-Viktorivna-Pashchenko <Nataliya-Viktorivna-Pashchenko@users.noreply.github.com>
2024-04-11 19:40:50 +0000

    deploy: 4677280d4491404a95cee57ca9fe61a9c4635a5d

---
+++ assets/Favorites-D4oO-Fsz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CEX8yEpJ.js";const s=()=>r.jsx("h1",{children:"Favorites"});export{s as default};
---
+++ assets/Teachers-Cf246hlk.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CEX8yEpJ.js";const s=()=>e.jsx("h1",{children:"Teachers"});export{s as default};

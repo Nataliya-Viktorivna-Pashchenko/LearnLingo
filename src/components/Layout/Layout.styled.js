4677280d4491404a95cee57ca9fe61a9c4635a5d
Nataliia Pashchenko <nvpashchenko@ukr.net>
2024-04-11 21:39:51 +0200

    add: pages

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyleMain = styled.div`
+ width: 1440px;
+
+    
+    .headerStyle {
+      margin-top:30px;
+      margin-left: 100px;
+      margin-right: 100px;
+      justify-content: space-between;
+    position: relative;
+    display: flex;
+    font-size: 25px;
+  }
+ 
+  .mainStyle{
+   overflow: scroll;
+   min-height:100vh
+  }  
+`;
